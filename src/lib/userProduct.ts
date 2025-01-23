8820f9035356ca7dcfe076a3fb2a7884732c92e7
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2025-01-23 23:52:35 +0500

    Error Handling & API Integration

--- src/lib/userProduct.ts
+++ src/lib/userProduct.ts
@@ -1,19 +1,21 @@
 import { client } from "./sanityClient";
 
 async function vercelProduct() {
-    const res = await fetch(`https://nextjs-glow-haven-e-commerce-store.vercel.app/`)
-    try {
-      if (res.status === 200) {
-        const result = await res.json();
-        return result;
-      } else {
-        console.log("Your cart is empty");
-        return null;
-      }
-    } catch (error) {
-      throw new Error(`Error: ${error}`);
+  try {
+    const res = await fetch(`https://nextjs-glow-haven-e-commerce-store.vercel.app/`);
+
+    if (res.status === 200) {
+      const result = await res.json();
+      return result;
+    } else {
+      console.log("Your cart is empty");
+      return null;
     }
+  } catch (error) {
+    // Re-throw the original error for handling higher up
+    throw error;
   }
+}
 const sanityProduct = async (product_id: string) => {
     const res = await client.fetch(`*[_type == "products" && _id == $product_id] {
       name,
