1c2c51c959ea59508f575123b8f1fc912b6256b3
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2025-02-02 23:06:33 +0500

    Added routing, installed missing types for swiper, and resolved peer dependency issues.

---
+++ components/section/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white">
+      {/** footer end  */}
+      <div className="copyright">
+        <p></p>
+        <p>
+          <span>© Copyright 2025</span>  GlowHaven E-commerce - All Rights Reserved. <span>Engr. Muhammad Adnan</span>
+        </p>
+        <p></p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
